451d81ea4ca34c626ac126c0676c0dc7cf6e8646
freekaja <freekaja@gmail.com>
2017-09-28 01:39:06 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Documentation is really important!
+// A “Hello World !” c++ code
+// Can be used to test compilers on machines
+
+#include <iostream>
+
+void print(const char *myname);
+
+int main()
+{
+    std::cout<<"Hello, World!\n";
+    return 0;
+}
